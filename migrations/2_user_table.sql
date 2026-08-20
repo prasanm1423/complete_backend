8b55252a3751fbea6ed6515b9b37eac25c90a94c
prasanm1423 <prasanmesta.1@gmail.com>
2026-08-20 19:30:24 +0530

    feat: add initial database schema migrations

---
+++ migrations/2_user_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash TEXT,
+    google_id VARCHAR(255) UNIQUE,
+    role VARCHAR(20) NOT NULL DEFAULT 'user' CHECK (role IN('user','admin'))
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+);
