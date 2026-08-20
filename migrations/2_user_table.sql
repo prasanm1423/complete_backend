a249a36a75e9642b69ad6df5c176915755cc8333
prasanm1423 <prasanmesta.1@gmail.com>
2026-08-20 19:33:34 +0530

    error:Syntax error of migration correceted

--- migrations/2_user_table.sql
+++ migrations/2_user_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users(
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash TEXT,
     google_id VARCHAR(255) UNIQUE,
-    role VARCHAR(20) NOT NULL DEFAULT 'user' CHECK (role IN('user','admin'))
+    role VARCHAR(20) NOT NULL DEFAULT 'user' CHECK (role IN('user','admin')),
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
 );
