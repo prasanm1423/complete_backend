450208cd314b34001f3509826b82c728f5045497
prasanm1423 <prasanmesta.1@gmail.com>
2026-08-21 21:55:11 +0530

    feat: add environment variable validation helper

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,9 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+//helper function
+function checkEnvVar(key: string): string {
+  const value = process.env[key];
+  if (!value) {
+    throw new Error(`missing env variables for ${key}`);
+  }
+  return value;
+}
 export const env = {
   port: Number(process.env.PORT ?? 4000),
   isProd: (process.env.NODE_ENV ?? "development") === "production",
   nodeEnv: process.env.NODE_ENV ?? "development",
   loglevel: process.env.LOG_LEVEL ?? "info",
+  databaseUrl: checkEnvVar("DATABASE_URL"),
 } as const;
